1f14eb5961c44ad0ee71e97325ce17030d507562
Nathaniel Mosco <natemosco@gmail.com>
2019-12-14 13:13:53 -0500

    building out schema within migrations file

--- data/migrations/20191214123709_projects_table.js
+++ data/migrations/20191214123709_projects_table.js
@@ -1,8 +1,37 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("Projects", table => {
+      table.increments();
+      table
+        .string("Name", 255)
+        .notNullable()
+        .unique();
+      table.text("Description", 1200);
+      table.boolean("Completed").defaultTo(false);
+    })
+    .createTable("Resources", table => {
+      table.increments();
+      table
+        .string("Name", 255)
+        .notNullable()
+        .unique();
+      table.text("Description", 1200);
+    })
+    .createTable("Project_Resources", table => {
+      table.primary(["Resource_ID", "Project_ID"]);
+      table
+        .integer("Resource_ID")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("Resources");
+      table
+        .integer("Project_Resources")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("Projects");
+    });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function(knex) {};
