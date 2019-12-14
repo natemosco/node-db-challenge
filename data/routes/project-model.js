bdccd74547fa8c25c694f2bec9c6f67515d13244
Nathaniel Mosco <natemosco@gmail.com>
2019-12-14 15:14:21 -0500

    almost done with helper functions in the project-model.js file

--- data/routes/project-model.js
+++ data/routes/project-model.js
@@ -2,16 +2,60 @@ const db = require("../db_config");
 
 module.exports = {
   findProjects,
-  findProjectById,
+  findProjectByID,
   addProjects,
   findProjectResources,
   findResources,
-  findResourceById,
+  findResourceByID,
   addResource,
   addTask,
   findTask
 };
 
-function findProjects(\) {
-  return db("projects");
+function findProjects() {
+  return db("Projects");
+}
+
+function findProjectByID(id) {
+  return db("Projects")
+    .where("id", "=", id)
+    .first();
+}
+
+function addProjects(projectData) {
+  return db("Projects")
+    .insert(projectData, "id")
+    .then(([id]) => {
+      return findProjectByID(id);
+    });
+}
+
+function findProjectResources(Project_ID) {
+  return db("Resources")
+    .select(
+      "Resources.id",
+      "Resources.Name",
+      "Resources.Description",
+      "Projects.Name"
+    )
+    .join("Projects", "Project.id", "=", "Resources.Project_ID")
+    .where("Resources.Project_ID", "=", Project_ID);
+}
+
+function findResources() {
+  return db("Resources");
+}
+
+function findResourceByID(id) {
+  return db("Resources")
+    .where("id", "=", id)
+    .first();
+}
+
+function addResource(newResource) {
+  return db("Resources")
+    .insert(newResource)
+    .then(([id]) => {
+      return findResourceByID(id);
+    });
 }
